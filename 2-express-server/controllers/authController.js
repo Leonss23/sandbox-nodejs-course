ef3360af95a22a6b0199629a81cccb093e257d57
Leonardo Gatti <leonss23esp@gmail.com>
2023-01-01 04:41:12 -0300

    jwt auth begun

--- 2-express-server/controllers/authController.js
+++ 2-express-server/controllers/authController.js
@@ -1,5 +1,9 @@
 const bcrypt = require("bcrypt");
 const { response } = require("express");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+const path = require("path");
 
 const usersDB = {
   users: require("../model/users.json"),
@@ -18,6 +22,25 @@ const handleLogin = async (request, response) => {
   if (!foundUser) return response.sendStatus(401);
   const match = await bcrypt.compare(password, foundUser.password);
   if (match) {
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    const otherUsers = usersDB.users.filter(
+      (user) => user.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    usersDB.users.push(currentUser);
+    await fsPromises(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
     response.json({ message: `Now logged in as ${username}.` });
   } else response.sendStatus(401);
 };
